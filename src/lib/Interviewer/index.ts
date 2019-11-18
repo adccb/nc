235b152b7630d836d68ef58481cccccfdcc23ed9
autumn grace <the@autumn.zone>
2019-11-17 19:07:36 -0500

    interviewer doesn\'t ask about one-tap trackers

--- src/lib/Interviewer/index.ts
+++ src/lib/Interviewer/index.ts
@@ -1,13 +1,16 @@
 import { question, confirmAndSubmit } from '../../util'
+import { isRange } from '../../types'
 import { StringDict } from '../../types'
 import { sanitizedOptions } from '../Settings'
 
 export const Interviewer = async (): Promise<boolean> => {
   console.log('starting interviewer...\n')
 
+  const agenda = sanitizedOptions.filter(isRange)
+
   const toSubmit: StringDict = {}
-  for (let i = 0; i < sanitizedOptions.length; i++) {
-    const { tag, flag, values } = sanitizedOptions[i]
+  for (let i = 0; i < agenda.length; i++) {
+    const { tag, flag, values } = agenda[i]
     const res = await question(`(${values}) how is your ${tag}? `)
     toSubmit[flag] = res
   }
